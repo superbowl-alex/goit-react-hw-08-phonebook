6eccfda296cf2a84e349e51f446f1986043369fc
Oleksandr <superbowl@ukr.net>
2022-11-15 15:26:38 +0200

    Initial commit 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from '@emotion/react';
+import App from 'components/App/App';
+import { store } from 'redux/store';
+import { theme } from 'theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
